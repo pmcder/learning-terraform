51a4808394148c58aefd62452510f6e6b6adfe1a
Patrick <67807576+pmcder@users.noreply.github.com>
2024-04-17 09:59:10 -0400

    updated subnet and vpc

--- main.tf
+++ main.tf
@@ -14,9 +14,18 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
+data "aws_vpc" "my-enablement-vpc" {
+  id = "vpc-06d8628a811699855"
+}
+
+data "aws_subnet" "enablement-sub1-private" {
+  id = "subnet-0c9b42badfe0ab24e"
+}
+
 resource "aws_instance" "web" {
   ami           = data.aws_ami.app_ami.id
   instance_type = "t3.nano"
+  subnet_id = data.aws_subnet.enablement-sub1-private.id
 
   tags = {
     Name = "HelloWorld"
